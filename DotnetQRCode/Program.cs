8b4365bc82cfd91c626e5a21764af7e8d7341e45
MuhammadOmerKhan <omer.khan@danatonline.com>
2023-01-21 00:53:01 +0500

    QR Code in .NET6.0

---
+++ DotnetQRCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+// Generate QR code
+string data = "Hello, QR code!";
+QRCodeGenerator qrGenerator = new QRCodeGenerator();
+QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+QRCode qrCode = new QRCode(qrCodeData);
+
+// Save QR code as PNG
+using (Bitmap qrCodeAsBitmap = qrCode.GetGraphic(20))
+{
+    qrCodeAsBitmap.Save("qrcode.png", ImageFormat.Png);
+}
